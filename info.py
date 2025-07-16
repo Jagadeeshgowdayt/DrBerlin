4149d2d9842869c2454442dfb2996f30cb6fd437
Jagadeesh S <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2025-07-16 12:13:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jagadeesh:jagadeesh@jagadeesh.zjibz.mongodb.net/?retryWrites=true&w=majority&appName=jagadeesh")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jagadeesh:jagadeesh@jagadeesh.v9joo7r.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "jagadeeshs")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'jagadeeshs')
 
